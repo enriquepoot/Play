9c55760fc43d90e695256e1af7018db985fe89ac
Enrique <kikepoot@gmail.com>
2012-10-20 17:34:26 -0500

    First Commit

---
+++ Play.Database/Schema Objects/Schemas/dbo/Tables/Persona.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Persona]
+(
+	ID int identity NOT NULL default newid(), 
+	Nombre varchar(300) NOT NULL,
+	Apellidos varchar(300) NOT NULL,
+	FechaNacimiento datetime NOT NULL,
+
+)
